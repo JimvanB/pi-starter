d8364c863813c575f03d13ef99d93979c58e431b
JimvanB <jvanbiezen@gmail.com>
2017-04-14 22:45:16 +0200

    dev - changed shit

--- src/main/java/biezen/jim/controller/StarterController.java
+++ src/main/java/biezen/jim/controller/StarterController.java
@@ -3,7 +3,10 @@ package biezen.jim.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by Jim on 14-4-2017.
@@ -22,6 +25,26 @@ public class StarterController {
     public String start() throws IOException {
         Process p = Runtime.getRuntime().exec("python /home/pi/dev/pi-motion/pi-motion/pi-surveillance.py --conf conf.json");
         return "Started";
+    }
+
+    public void runProgram(String program) throws InterruptedException, IOException
+    {
+        Process proc = Runtime.getRuntime().exec (program);
+        InputStream progOutput = proc.getInputStream ();
+        InputStreamReader inputReader=new InputStreamReader(progOutput);
+        BufferedReader reader = new BufferedReader(inputReader);
+        String line;
+        while ((line = reader.readLine()) != null)
+        {
+            System.out.println(line);
+        }
+        if (0 == proc.waitFor ()) {
+            System.out.println("Process completed successfully");
+        }
+        else
+        {
+            System.out.println("Their was some issue while running the program");
+        }
     }
 
 }
