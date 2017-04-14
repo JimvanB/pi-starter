681f0ea377d0613eec8f6bc436a5ffc025ed116f
JimvanB <jvanbiezen@gmail.com>
2017-04-14 22:38:26 +0200

    dev - changed shit

--- src/main/java/biezen/jim/controller/StarterController.java
+++ src/main/java/biezen/jim/controller/StarterController.java
@@ -1,34 +1,27 @@
 package biezen.jim.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 /**
  * Created by Jim on 14-4-2017.
  */
-@Controller
+
+
+@RestController
 public class StarterController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
 
     @RequestMapping("/start")
     public String start() throws IOException {
         Process p = Runtime.getRuntime().exec("python /home/pi/dev/pi-motion/pi-motion/pi-surveillance.py --conf conf.json");
         return "Started";
     }
-
-    @RequestMapping("/stop")
-    public String stop(){
-        return "Stop";
-    }
-
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
-
 
 }
